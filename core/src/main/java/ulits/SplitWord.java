ea71cd70426aef50cd488e82a4fd48c9c2a881a6
bassnsmq <2418267675@qq.com>
2019-07-12 10:13:30 +0800

    扫描项目的路径请不要带有中文，否则会重构失败

--- core/src/main/java/ulits/SplitWord.java
+++ core/src/main/java/ulits/SplitWord.java
@@ -45,7 +45,7 @@ public class SplitWord {
        }
          return out;
     }
-/*
+
     public List<String> readData() throws IOException {
         List<String> dataList = new ArrayList<>();
         InputStreamReader fileReader = new InputStreamReader(FileUlits.readFilesByClassLoader("static/wordninja_words.txt"));
@@ -58,72 +58,72 @@ public class SplitWord {
         bufferedReader.close();
         fileReader.close();
         return  dataList;
-    }*/
-
-    public List<String> readData() throws IOException {
-        int maxBtye = 1024;
-        List<String> dataList = new ArrayList<>();
-        File file=FileUlits.readSourceFile("static/wordninja_words.txt");
-        //获取文件管道
-        FileChannel fc = new FileInputStream(file).getChannel();
-        byte changeLine = 10;
-        //换行符前面字符组合，可解决乱码
-        ByteBuffer rBuffer = ByteBuffer.allocate(maxBtye);
-        ByteBuffer elseBuffer = ByteBuffer.allocate(maxBtye*3);
-        //将缓冲区中数据读到字符数组
-        byte[] bs=null;
-        while (fc.read(rBuffer)!=-1){
-            // 根据读取到的字节数确定即将获取的字节数组大小
-            bs = new byte[rBuffer.position()];
-            rBuffer.rewind();
-            // 相对读，从position位置读取一个byte[]
-            rBuffer.get(bs);
-            // 清楚缓冲区，供下次使用
-            rBuffer.clear();
-            for (byte b : bs) {
-                // 遭遇换行符
-                if (b == changeLine) {
-                    // 字符缓冲区位置，用于确定字节数组大小
-                    int byteSize = elseBuffer.position();
-                    // 确定出字节数组大小
-                    byte[] byteLine = new byte[byteSize];
-                    // position=0 准备读取数据
-                    elseBuffer.rewind();
-                    // 从position开始读取byteLine.length个字节
-                    elseBuffer.get(byteLine);
-                    // 如果是 剔除 /r 前的 /n 后转换为字符串
-                    String line = new String(byteLine, 0, byteLine.length - 1);
-                    //    添加到返回集合中
-                    dataList.add(line);
-                    elseBuffer.clear();
-                    continue;
-                }
-                try {
-                    // 缓存住未匹配上的数据
-                    elseBuffer.put(b);
-                    //    由于提前确定的字节缓冲区太小导致错误
-                } catch (BufferOverflowException e) {
-                   e.printStackTrace();
-                    return null;
-                }
-
-            }
-        }
-        //    循环完毕，处理剩余数据,注释同上
-        if (elseBuffer.position() > 0) {
-            int byteSize = elseBuffer.position();
-            byte[] byteLine = new byte[byteSize];
-            elseBuffer.rewind();
-            elseBuffer.get(byteLine);
-            String line = new String(byteLine, 0, byteLine.length);
-            dataList.add(line);
-            elseBuffer.clear();
-        }
-        fc.close();
-        rBuffer.clear();
-        maxlength=dataList.get(maxList(dataList)).length();
-        return dataList;
     }
+
+//    public List<String> readData() throws IOException {
+//        int maxBtye = 1024;
+//        List<String> dataList = new ArrayList<>();
+//        File file=FileUlits.readSourceFile("static/wordninja_words.txt");
+//        //获取文件管道
+//        FileChannel fc = new FileInputStream(file).getChannel();
+//        byte changeLine = 10;
+//        //换行符前面字符组合，可解决乱码
+//        ByteBuffer rBuffer = ByteBuffer.allocate(maxBtye);
+//        ByteBuffer elseBuffer = ByteBuffer.allocate(maxBtye*3);
+//        //将缓冲区中数据读到字符数组
+//        byte[] bs=null;
+//        while (fc.read(rBuffer)!=-1){
+//            // 根据读取到的字节数确定即将获取的字节数组大小
+//            bs = new byte[rBuffer.position()];
+//            rBuffer.rewind();
+//            // 相对读，从position位置读取一个byte[]
+//            rBuffer.get(bs);
+//            // 清楚缓冲区，供下次使用
+//            rBuffer.clear();
+//            for (byte b : bs) {
+//                // 遭遇换行符
+//                if (b == changeLine) {
+//                    // 字符缓冲区位置，用于确定字节数组大小
+//                    int byteSize = elseBuffer.position();
+//                    // 确定出字节数组大小
+//                    byte[] byteLine = new byte[byteSize];
+//                    // position=0 准备读取数据
+//                    elseBuffer.rewind();
+//                    // 从position开始读取byteLine.length个字节
+//                    elseBuffer.get(byteLine);
+//                    // 如果是 剔除 /r 前的 /n 后转换为字符串
+//                    String line = new String(byteLine, 0, byteLine.length - 1);
+//                    //    添加到返回集合中
+//                    dataList.add(line);
+//                    elseBuffer.clear();
+//                    continue;
+//                }
+//                try {
+//                    // 缓存住未匹配上的数据
+//                    elseBuffer.put(b);
+//                    //    由于提前确定的字节缓冲区太小导致错误
+//                } catch (BufferOverflowException e) {
+//                   e.printStackTrace();
+//                    return null;
+//                }
+//
+//            }
+//        }
+//        //    循环完毕，处理剩余数据,注释同上
+//        if (elseBuffer.position() > 0) {
+//            int byteSize = elseBuffer.position();
+//            byte[] byteLine = new byte[byteSize];
+//            elseBuffer.rewind();
+//            elseBuffer.get(byteLine);
+//            String line = new String(byteLine, 0, byteLine.length);
+//            dataList.add(line);
+//            elseBuffer.clear();
+//        }
+//        fc.close();
+//        rBuffer.clear();
+//        maxlength=dataList.get(maxList(dataList)).length();
+//        return dataList;
+//    }
     public Map<String,Double> listToMap(List<String> dataList){
         dataMap = new HashMap<String, Double>();
         for(int i = 0;i<dataList.size();i++){
